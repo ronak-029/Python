bf0ad80fb64174aab2e199e5c5d8e8616a0a9eb8
Ronak Kumar <kastudentbca@gmail.com>
2026-09-22 22:34:28 +0530

    chapter_5

---
+++ python/chapter_5/if_else.py
@@ -0,0 +1,58 @@
+# Writ a program to find greatest of four numbers entered by the user.
+
+# num1 = int(input("Enter Number 1 : "))
+# num2 = int(input("Enter Number 2 : "))
+# num3 = int(input("Enter Number 3 : "))
+# num4 = int(input("Enter Number 4 : "))
+
+
+# if(num1>num2 and num1>num3 and num1>num4):
+#     print(f"num1 is Greater Than all Number {num1}")
+
+# elif(num2>num1 and num2>num3 and num2>num4):
+#     print(f"num2 is Greater Than all Number {num2}")
+
+# elif(num3>num1 and num3>num2 and num3>num4):
+#     print(f"num is Greater Than all Number {num3}")
+
+# else:
+#     print(f"num4 is Greater Than all Number {num4}")
+# --------------------------------------------------------------------------------------------
+# Write a program to find out whether a student
+# is pass bro' fail if it regires total 40% and
+# st least 33), in each subject to pass. Asune
+# 3 Sulyects and take morks as an input from the user
+
+# Marks1 = int(input("Enter Number 1 : "))
+# marks2 = int(input("Enter Number 2 : "))
+# Marks3 = int(input("Enter Number 3 : "))
+
+# total_percentage = (100*(Marks1+marks2+Marks3))/300
+
+# if(total_percentage>=40):
+#     print(f"You are Pass Bro :{total_percentage}")
+# else:
+#     print(f"You are Fail Better luck Next time : {total_percentage}")
+
+# ----------------------------------------------------------------------------------------------------
+
+
+# make a lot of money, buy now, I
+# now sulycribe this
+# "click this" Write a propram to deteet these spams.
+
+# p1 = "make a lot of money"
+# p2 = "buy now"
+# p3 = "now sulycribe this"
+# p4 = "click this" 
+
+# message = input("Enter Your Message")
+
+# if(p1 in message or p2 in message or p3 in message or p4 in message):
+#     print("This is a Spam Message")
+
+# else:
+#     print("this is not spam Message")
+
+# ----------------------------------------------------------------------------------------------------
+
---
+++ python/chapter_5/loops.py
@@ -0,0 +1,24 @@
+# i = 1
+# while(i<10):
+#     print(i*2)
+#     i += 1
+
+# for i in range (1,9,2):
+#     print(i)
+
+# n = int(input("Enter a number : "))
+
+# for i in range (1,11):
+#     print(f"{n} X {i} = {n*i}")
+
+
+# i = 1
+# fact = 1
+# for i in range(1 , n+1):
+#     fact *= i
+
+# print(f"the factorial of {n} is : {fact}")
+
+n = int(input("Enter a number : "))
+for i in range (1,11):
+    print(f"{n} X {11-i} = {n*(11-i)}")
