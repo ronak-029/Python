dc89922de8002fe710cd73af150c2905fc76eccb
Ronak Kumar <kastudentbca@gmail.com>
2026-09-24 20:06:06 +0530

    problem_statement

---
+++ python/chapter_6/problem_statement.py
@@ -0,0 +1,48 @@
+# def avg():
+#     a = int(input("Enter You Number : "))
+#     b = int(input("Enter You Number : "))
+#     c = int(input("Enter You Number : "))
+
+#     average = (a+b+c)/3
+#     print(average)
+
+#     if(average%2 == 0):
+#         print(f"Your Averager is divied by 2 {average}")
+#     else:
+#         print(f"Your Average is not divide By 2 {average}")
+# avg()    
+# 
+# a = int(input("Enter You Number : "))
+# b = int(input("Enter You Number : "))
+# c = int(input("Enter You Number : "))  
+
+# def gra(a,b,c):
+    
+#     if(a>b and a>c):
+#          print(f"This number is greater than All : {a}")
+   
+#     elif(b>a and b>c):
+#          print(f"This number is greater than All : {b}")
+    
+#     else:
+#          print(f"This number is greater than All : {c}")
+
+
+  
+# gra(a,b,c)
+
+# n = int(input("Enter a number : "))
+# def pattern(n):
+#     if(n==0):
+#         return
+#     print("*"  * n)
+#     pattern(n-1)
+
+# pattern(n)
+
+# n = int(input("Enter a number : "))
+# def table(n):
+#     for i in range(1,11):
+#         print(f"{n} X {i} = {n*i}")
+
+# table(n)
