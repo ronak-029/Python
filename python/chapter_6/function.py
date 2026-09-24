c8ed7cc2e197bd1a258a248c0d0732b9590510db
Ronak Kumar <kastudentbca@gmail.com>
2026-09-24 19:30:41 +0530

    chapter_6

---
+++ python/chapter_6/function.py
@@ -0,0 +1,30 @@
+# def sum(a,b):
+#     print(a+b)
+
+# sum(1,4)
+# sum(1,9)
+# sum(9,4)
+
+# def great(name,ending):
+#     print(f"good Morning {name}")
+#     print(ending)
+
+# great("Ronak Singh", "Thankyou")
+# great("Suraj", "Thankyou")
+# great("Sameer", "Thankyou")
+
+# def avg():
+#     a = int(input("Enter You Number : "))
+#     b = int(input("Enter You Number : "))
+#     c = int(input("Enter You Number : "))
+
+#     average = (a+b+c)/3
+#     print(average)
+
+#     if(average%2 == 0):
+#         print(f"Your Averager is divied by 2 {average}")
+#     else:
+#         print(f"Your Average is not divide By 2 {average}")
+# avg()        
+
+
